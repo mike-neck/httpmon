effe74f735eb9faedc92453f5e12a09b9f14ecab
mike_neck <jkrt3333@gmail.com>
2020-01-29 22:58:06 +0900

    define interface

--- http_client.go
+++ http_client.go
@@ -2,3 +2,42 @@ package httpmon
 
 type HttpMon struct {
 }
+
+type HttpMethod string
+
+type HttpHeaders map[string][]string
+
+type Queries map[string][]string
+
+type HttpTestRequest interface {
+	Method() HttpMethod
+	Headers() HttpHeaders
+	URL() string
+	Queries() Queries
+}
+
+type TestResult int
+
+const (
+	TestSuccess TestResult = iota
+	TestFailure
+)
+
+type HttpStatus int
+
+type HttpHeaderName string
+
+type HttpHeaderValue string
+
+type HttpBodyTester func(body []byte) (result bool, err error)
+
+type HttpTest interface {
+	ExpectStatus(status HttpStatus) TestResult
+	ExpectHeader(name HttpHeaderName) HttpHeaderTest
+	ExpectBody(tester HttpBodyTester) TestResult
+}
+
+type HttpHeaderTest interface {
+	Exists() TestResult
+	HasValue(value HttpHeaderValue) TestResult
+}
